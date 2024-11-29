7935186556c3379d6d86e35db3eff9478b388207
OPOLO <106403765+opolonix@users.noreply.github.com>
2024-11-29 16:27:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+log = console.log;
+
+
+document.querySelectorAll(".radio").forEach(el => {
+    el.addEventListener("click", e => {
+        let index = el.attributes.index.value;
+        document.body.style.setProperty('--paramsIndex', el.attributes.index.value);
+        
+        if (index == '0') {
+            document.querySelector(".positions").classList.remove('hidden')
+            document.querySelector(".complited").classList.add('hidden')
+        }
+        else {
+            document.querySelector(".complited").classList.remove('hidden')
+            document.querySelector(".positions").classList.add('hidden')
+        }
+    })
+})
