b22c41309219a31b967ee6e4053a002ac3c5a0de
opolonix <misha90638@gmail.com>
2024-12-07 15:07:13 +0300

    исправлены баги

--- tg.js
+++ tg.js
@@ -4,3 +4,10 @@ const log = console.log;
 
 tg.headerColor = "#1f0505";
 tg.bottomBarColor = "#161616";
+
+if (history.length != 0) {
+    tg.BackButton.show();
+    BackButton.onClick(() => {
+        history.back();
+    });
+}
