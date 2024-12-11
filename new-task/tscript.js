754d2026af126414c70c3aca7c670511aa05b10f
opolonix <misha90638@gmail.com>
2024-12-11 23:34:46 +0300

    исправлен прогресс бар для страницы создания тикета

--- new-task/tscript.js
+++ new-task/tscript.js
@@ -1,10 +1,15 @@
 const progress = document.querySelector(".progress");
 const dot = document.querySelector(".dot");
 
-const handleProgress = (x) => {
-    offset = (window.innerWidth - progress.clientWidth) / 2
-    target = (x-offset) / progress.clientWidth * 100
-    target = Math.max(Math.min(target, 100), 0)
+const handleProgress = (x, is_position = true) => {
+    if (is_position) {
+        offset = (window.innerWidth - progress.clientWidth) / 2
+        target = (x-offset) / progress.clientWidth * 100
+        target = Math.max(Math.min(target, 100), 0)
+    }
+    else target = x
+
+    setConfigOption('progress', target)
     progress.style.setProperty("--progress", target)
 
     document.querySelectorAll(".point").forEach(point => {
@@ -14,17 +19,33 @@ const handleProgress = (x) => {
 }
 
 let draggeble = false;
-
-progress.addEventListener("touchstart", e => handleProgress(e.touches[0].clientX))
-progress.addEventListener("touchmove", e => handleProgress(e.touches[0].clientX))
+const points = [0, 100 / 3, (100 / 3) * 2, 100];
+progress.addEventListener("touchstart", e => {
+    progress.classList.remove('base')
+    handleProgress(e.touches[0].clientX)
+}, {passive: true})
+progress.addEventListener("touchmove", e => handleProgress(e.touches[0].clientX), {passive: true})
+progress.addEventListener("touchend", e => {
+    
+    progress.classList.add('base')
+    let value = parseFloat(progress.style.getPropertyValue("--progress"))
+    handleProgress(points.reduce((prev, curr) => Math.abs(curr - value) < Math.abs(prev - value) ? curr : prev), false)
+    
+}, {passive: true})
 progress.addEventListener("mousemove", e => {
     if (draggeble) handleProgress(e.pageX);
-})
+}, {passive: true})
 progress.addEventListener("mousedown", e => {
-    draggeble = true;
+    draggeble = true; 
+    progress.classList.remove('base')
     handleProgress(e.pageX);
-})
-progress.addEventListener("mouseup", e => draggeble = false)
+}, {passive: true})
+progress.addEventListener("mouseup", e => {
+    draggeble = false
+    progress.classList.add('base')
+    let value = parseFloat(progress.style.getPropertyValue("--progress"))
+    handleProgress(points.reduce((prev, curr) => Math.abs(curr - value) < Math.abs(prev - value) ? curr : prev), false)
+}, {passive: true})
 
 document.querySelectorAll(".selection").forEach(selects => {
     let buttons = selects.querySelectorAll("button");
